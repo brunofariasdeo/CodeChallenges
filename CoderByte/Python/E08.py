d20494a4507b72b64855db81cdcd0c50c5e2afdd
Bruno de Oliveira <jbfo@cin.ufpe.br>
2019-08-01 04:21:18 -0300

    E08 completed

---
+++ CoderByte/Python/E08.py
@@ -0,0 +1,11 @@
+def CheckNums(num1,num2): 
+
+    if num2 > num1:
+        return "true"
+    elif num2 == num1:
+        return -1
+    else:
+        return "false"
+
+# keep this function call here  
+print (CheckNums(122,12))  
