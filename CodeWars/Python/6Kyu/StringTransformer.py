f9c4c7593e5c35face52fe0df93b97a9bc4424b7
Bruno Oliveira <jbfo@cin.ufpe.br>
2019-08-16 17:23:53 -0300

    .

---
+++ CodeWars/Python/6Kyu/StringTransformer.py
@@ -0,0 +1,31 @@
+# Link: https://www.codewars.com/kata/5878520d52628a092f0002d0
+
+# Given a string, return a new string that has transformed based on the input:
+
+# Change case of every character, ie. lower case to upper case, upper case to lower case.
+# Reverse the order of words from the input.
+# Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+# For example:
+
+# "Example Input" ==> "iNPUT eXAMPLE"
+# You may assume the input only contain English alphabet and spaces.
+
+def string_transformer(s):
+    newString = ""
+    stringList = []
+    for letter in s:
+        if letter.islower():
+            newString += letter.upper()
+        elif letter.isupper():
+            newString += letter.lower()
+        else:
+            newString += letter
+            stringList.append(newString)
+            newString = ""
+ 
+    stringList.append(newString)
+    
+    return ' '.join(list(reversed(stringList)))[:-1]
+
+print(string_transformer("You Know When  THAT  Hotline Bling"))
