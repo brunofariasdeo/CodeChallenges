2d41c8be1f2fb6b816152a66208f385b56a2b9da
Bruno <jbfo@cin.ufpe.br>
2019-09-07 21:09:25 -0300

    Nut tree aehoo

--- CodeWars/Python/6Kyu/NutFarm.py
+++ CodeWars/Python/6Kyu/NutFarm.py
@@ -1,9 +1,31 @@
+# Link: https://www.codewars.com/kata/nut-farm/train/python
+
 def shake_tree(tree):
-    for index, item in enumerate(tree):
-        index+=2
-        print(index)
-    # return '\n'.join(tree)
+
+    position = (len(tree[0]))*[0]
+
+    for index, row in enumerate(tree):
+        for innerIndex, item in enumerate(row):
+            index = 0
+            if item == "o":
+                nutFound = True
+                while nutFound == True:
+                    if index+1 < len(tree):
+                        index+=1
+                        if tree[index][innerIndex] == "/":
+                            if innerIndex+1<= len(row):
+                                innerIndex-=1
+                        elif tree[index][innerIndex] == "\\":
+                            if innerIndex+1<= len(row):
+                                innerIndex+=1
+                        elif tree[index][innerIndex] == "_":
+                            break
+
+                    if index == len(tree)-1:
+                        nutFound = False
+                        position[innerIndex] += 1
+
+    return position
 
 a = [' o o o  ', ' /    / ', '   /    ', '  /  /  ', '   ||   ', '   ||   ', '   ||   ']
-
 print(shake_tree(a))
