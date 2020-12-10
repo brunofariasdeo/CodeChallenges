ea01ba2a4d3724ed1b8ec3775394387820b9f2b9
Bruno Oliveira <bruno.oliveira@liferay.com>
2020-12-10 20:56:43 -0300

    Finished 8 Kyu - BegginerSeries#2 - Java

---
+++ CodeWars/Java/8Kyu/BegginerSeries2.java
@@ -0,0 +1,7 @@
+// URL: https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/java
+
+public class BegginerSeries2 {
+    public static int Past(int h, int m, int s) {
+        return ((h * 3600) + (m * 60) + s) * 1000;
+    }
+}
