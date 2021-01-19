97f6c729ac1cbe78904e6ac640456e77193d7ac7
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-01-19 19:12:22 -0300

    Finished 6 Kyu - Multiples of 3 or 5 - Java

---
+++ CodeWars/Java/6Kyu/MultiplesOf3Or5.java
@@ -0,0 +1,15 @@
+// URL: https://www.codewars.com/kata/514b92a657cdc65150000006/train/java
+
+public class MultiplesOf3Or5 {
+  public int solution(int number) {
+    int sum = 0;
+
+    for (int i = 1; i < number; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        sum += i;
+      }
+    }
+
+    return sum;
+  }
+}
